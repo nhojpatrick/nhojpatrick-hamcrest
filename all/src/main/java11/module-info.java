0c1aa805b0f0660d822b9a6f53623248d874b69d
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-02-05 00:10:18 +0000

    IsJavaSqlTime impl

--- all/src/main/java11/module-info.java
+++ all/src/main/java11/module-info.java
@@ -1,6 +1,7 @@
 module com.github.nhojpatrick.hamcrest.all {
     requires transitive com.github.nhojpatrick.hamcrest.collections;
     requires transitive com.github.nhojpatrick.hamcrest.datetime;
+    requires transitive com.github.nhojpatrick.hamcrest.datetime.java.sql;
     requires transitive com.github.nhojpatrick.hamcrest.lang;
     requires transitive com.github.nhojpatrick.hamcrest.optionals;
 }
