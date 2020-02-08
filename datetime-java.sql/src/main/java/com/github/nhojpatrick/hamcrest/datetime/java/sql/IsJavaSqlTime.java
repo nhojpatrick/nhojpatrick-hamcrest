e58c7ebc5fbde48e7af36ba5014869db08db871d
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2020-02-08 16:56:22 +0000

    IsJavaSqlTime After/Before impl

--- datetime-java.sql/src/main/java/com/github/nhojpatrick/hamcrest/datetime/java/sql/IsJavaSqlTime.java
+++ datetime-java.sql/src/main/java/com/github/nhojpatrick/hamcrest/datetime/java/sql/IsJavaSqlTime.java
@@ -1,5 +1,8 @@
 package com.github.nhojpatrick.hamcrest.datetime.java.sql;
 
+import com.github.nhojpatrick.hamcrest.datetime.flags.CompareType;
+import com.github.nhojpatrick.hamcrest.datetime.java.sql.internal.after.IsAfterJavaSqlTimeImpl;
+import com.github.nhojpatrick.hamcrest.datetime.java.sql.internal.before.IsBeforeJavaSqlTimeImpl;
 import com.github.nhojpatrick.hamcrest.datetime.java.sql.internal.equals.IsEqualJavaSqlTimeImpl;
 import org.hamcrest.Matcher;
 import org.slf4j.Logger;
@@ -7,11 +10,36 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.Time;
 
+import static com.github.nhojpatrick.hamcrest.datetime.flags.CompareType.EXCLUSIVE;
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 
 public final class IsJavaSqlTime {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IsJavaSqlTime.class);
+
+    public static <T extends Time> Matcher<T> afterJavaSqlTime(final T after) {
+        LOGGER.debug("IsJavaSqlTime#afterJavaSqlTime(<T> {})", after);
+        return doAfterJavaSqlTime(after, EXCLUSIVE);
+    }
+
+    public static <T extends Time> Matcher<T> afterJavaSqlTime(final T after,
+                                                               final CompareType compareType) {
+        LOGGER.debug("IsJavaSqlTime#afterJavaSqlTime(<T> {}, (CompareType) {})",
+                after, compareType);
+        return doAfterJavaSqlTime(after, compareType);
+    }
+
+    public static <T extends Time> Matcher<T> beforeJavaSqlTime(final T before) {
+        LOGGER.debug("IsJavaSqlTime#beforeJavaSqlTime(<T> {})", before);
+        return doBeforeJavaSqlTime(before, EXCLUSIVE);
+    }
+
+    public static <T extends Time> Matcher<T> beforeJavaSqlTime(final T before,
+                                                                final CompareType compareType) {
+        LOGGER.debug("IsJavaSqlTime#beforeJavaSqlTime(<T> {}, (CompareType) {})",
+                before, compareType);
+        return doBeforeJavaSqlTime(before, compareType);
+    }
 
     public static <T> Matcher<T> javaSqlTime() {
         LOGGER.debug("IsJavaSqlTime#javaSqlTime()");
@@ -21,6 +49,20 @@ public final class IsJavaSqlTime {
     public static <T extends Time> Matcher<T> javaSqlTime(final Matcher<T> matcher) {
         LOGGER.debug("IsJavaSqlTime#javaSqlTime(Matcher<T> {})", matcher);
         return new IsEqualJavaSqlTimeImpl(matcher);
+    }
+
+    private static <T extends Time> Matcher<T> doAfterJavaSqlTime(final T before,
+                                                                  final CompareType compareType) {
+        LOGGER.debug("IsJavaSqlTime#doAfterJavaSqlTime((After) {}, (CompareType) {})",
+                before, compareType);
+        return new IsAfterJavaSqlTimeImpl(before, compareType);
+    }
+
+    private static <T extends Time> Matcher<T> doBeforeJavaSqlTime(final T before,
+                                                                   final CompareType compareType) {
+        LOGGER.debug("IsJavaSqlTime#doBeforeJavaSqlTime((Before) {}, (CompareType) {})",
+                before, compareType);
+        return new IsBeforeJavaSqlTimeImpl(before, compareType);
     }
 
     IsJavaSqlTime() {
---
+++ datetime-java.sql/src/main/java/com/github/nhojpatrick/hamcrest/datetime/java/sql/internal/after/IsAfterJavaSqlTimeImpl.java
@@ -0,0 +1,76 @@
+package com.github.nhojpatrick.hamcrest.datetime.java.sql.internal.after;
+
+import com.github.nhojpatrick.hamcrest.datetime.flags.CompareType;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Time;
+
+import static java.util.Objects.isNull;
+
+public class IsAfterJavaSqlTimeImpl<T extends Time>
+        extends TypeSafeMatcher<T> {
+
+    public static final String SUPPLIED_COMPARE_TYPE_MUST_NOT_BE_NULL = "Supplied After CompareType must not be null";
+    public static final String SUPPLIED_EXPECTED_MUST_NOT_BE_NULL = "Supplied After must not be null";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IsAfterJavaSqlTimeImpl.class);
+
+    protected final T after;
+    protected final CompareType compareType;
+
+    public IsAfterJavaSqlTimeImpl(final T after,
+                                  final CompareType compareType) {
+        LOGGER.debug("IsAfterJavaSqlTimeImpl((After) {}, (CompareType) {}, (RoundingType) {})",
+                after, compareType);
+
+        if (isNull(after)) {
+            throw new IllegalArgumentException(SUPPLIED_EXPECTED_MUST_NOT_BE_NULL);
+        }
+        if (isNull(compareType)) {
+            throw new IllegalArgumentException(SUPPLIED_COMPARE_TYPE_MUST_NOT_BE_NULL);
+        }
+
+        this.after = after;
+        this.compareType = compareType;
+    }
+
+    @Override
+    public void describeTo(final Description description) {
+
+        String textDescription = "";
+        switch (this.compareType) {
+            case INCLUSIVE:
+                textDescription = "after or equal to ";
+                break;
+
+            case EXCLUSIVE:
+            default:
+                textDescription = "after ";
+                break;
+        }
+
+        description
+                .appendText(textDescription)
+                .appendValue(this.after);
+    }
+
+    @Override
+    protected boolean matchesSafely(final T item) {
+
+        boolean matchesSafely = item.after(this.after);
+
+        switch (this.compareType) {
+            case INCLUSIVE:
+                final boolean notBefore = !item.before(this.after);
+                matchesSafely |= notBefore;
+                break;
+            default:
+        }
+
+        return matchesSafely;
+    }
+
+}
